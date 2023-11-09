bde284424b830d73be0c2c464dbfc0a11d048b7f
Jeylson0 <jhonalson.lugada@gmail.com>
2023-11-10 02:50:15 +0800

    Version 1.1.0: Add card feat:
    Add card form takes input from user and place data into a new instance.
    New instance pushed to library arr which displays data on HTML element

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,21 @@
+/* DOM */
+const submitButton = document.querySelector('.submit-button')
+const closeButton = document.querySelector('.cancel-button')
+const formTitle = document.querySelector('#formTitle');
+const formAuthor = document.querySelector('#formAuthor');
+const formPages = document.querySelector('#formPages');
+const addForm = document.querySelector('.add-form')
+const input = document.querySelectorAll('input[type="text"]') //array of elements
+const addButton = document.querySelector('.add-btn');
+const modal = document.querySelector('dialog');
+
 const library = []
 
+let titleData;
+let authorData;
+let pageData
+
+/* Obj and Arr */
 function Book(name, author, pages) {
     this.name = name;
     this.author = author;
@@ -8,16 +24,63 @@ function Book(name, author, pages) {
 
 function addBook() {
     let bookObj = new Book;
-    let bookName = prompt('Book Title:');
-    let bookAuthor = prompt('Author:');
-    let bookPages = prompt('Pages:')
-    bookObj.name = bookName;
+    let bookTitle = titleData;
+    let bookAuthor = authorData;
+    let bookPages = pageData;
+    bookObj.name = bookTitle;
     bookObj.author = bookAuthor;
     bookObj.pages = bookPages;
     library.push(bookObj)
+    displayLibrary();
 }
-
 function displayLibrary() {
 
+    library.forEach((books, i)=> {
+        console.log(`${books.name}, ${books.author}, ${books.pages}, ${i}`);
+        let cardTitle = document.querySelector(`.main div:nth-child(${i+1}) .book-title`);
+        let cardAuthor = document.querySelector(`.main div:nth-child(${i+1}) .book-author`);
+        let cardPages = document.querySelector(`.main div:nth-child(${i+1}) .book-pages`);
+        cardTitle.textContent = books.name;
+        cardAuthor.textContent = books.author;
+        cardPages.textContent = books.pages;
+    })
 }
+
+/* Event listener */
+addButton.addEventListener('click', () => {
+    modal.showModal();
+
+    input.forEach((element) => {
+        element.addEventListener('change', () => {
+            if(element == formTitle) {
+                titleData = element.value
+            }
+            if(element == formAuthor) {
+                authorData = element.value;
+            }
+            if(element == formPages) {
+                pageData = element.value
+            }
+        })
+    })
+
+    closeButton.addEventListener('click', () => {
+        modal.close()
+    })
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        addBook();
+        input.forEach((element) => {
+            element.value = ''
+        })
+        modal.close()
+    })
+
+})
+
+
+
+
+
 
